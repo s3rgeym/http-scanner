6e22eb7a06cf9bab3988d13825247d44f4a2251c
None <yamldeveloper@proton.me>
2024-09-23 20:10:17 +0300

    переименование

--- main.go
+++ main.go
@@ -127,7 +127,7 @@ func main() {
 					return
 				}
 
-				for key, value := range getDefaultHeaders() {
+				for key, value := range getRequestHeaders() {
 					req.Header.Set(key, value)
 				}
 
@@ -229,7 +229,7 @@ func main() {
 	log.Infof("Scanning finished!")
 }
 
-func getDefaultHeaders() map[string]string {
+func getRequestHeaders() map[string]string {
 	return map[string]string{
 		"Accept-Language": "en-US,en;q=0.9",
 		"Accept":          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
